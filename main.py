b842bddfcf9a5d420c5cd78a452c1f63b83164b6
RonaldFisher9999 <1993sabre@gmail.com>
2023-04-18 11:44:14 +0900

    fix args

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ def define_argparser():
     parser = argparse.ArgumentParser(description='parser')
     
     ############### BASIC OPTION
-    parser.add_argument('--model', type=str, choices=['FM', 'FFM', 'NCF', 'WDN', 'DCN', 'CNN_FM', 'DeepCoNN'],
-                                help='학습 및 예측할 모델을 선택할 수 있습니다.', required=True)
-    parser.add_argument('--data_path', type=str, default='data/', help='Data path를 설정할 수 있습니다.')
+    parser.add_argument('--data_path', type=str, default='/opt/ml/data/', help='Data path를 설정할 수 있습니다.')
     parser.add_argument('--saved_model_path', type=str, default='./saved_models', help='Saved Model path를 설정할 수 있습니다.')
-    parser.add_argument('--data_shuffle', type=bool, default=True, help='데이터 셔플 여부를 조정할 수 있습니다.')
+    parser.add_argument('--model', type=str, choices=['FM', 'FFM', 'DeepFFM', 'NCF', 'WDN', 'DCN', 'CNN_FM', 'DeepCoNN'],
+                                help='학습 및 예측할 모델을 선택할 수 있습니다.')
+    parser.add_argument('--data_shuffle', type=int, default=1, help='데이터 셔플 여부를 조정할 수 있습니다.')
     parser.add_argument('--test_size', type=float, default=0.2, help='Train/Valid split 비율을 조정할 수 있습니다.')
+    parser.add_argument('--stratify', type=int, default=0, help='rating에 따라 stratified split할지 여부를 조정합니다. default=0')
     parser.add_argument('--seed', type=int, default=42, help='seed 값을 조정할 수 있습니다.')
-    parser.add_argument('--use_best_model', type=bool, default=True, help='검증 성능이 가장 좋은 모델 사용여부를 설정할 수 있습니다.')
+    parser.add_argument('--use_best_model', type=int, default=1, help='검증 성능이 가장 좋은 모델 사용여부를 설정할 수 있습니다.')
+
 
     ############### TRAINING OPTION
     parser.add_argument('--batch_size', type=int, default=1024, help='Batch size를 조정할 수 있습니다.')
@@ -29,17 +31,21 @@ def define_argparser():
     parser.add_argument('--optimizer', type=str, default='ADAM', choices=['SGD', 'ADAM'], help='최적화 함수를 변경할 수 있습니다.')
     parser.add_argument('--weight_decay', type=float, default=1e-6, help='Adam optimizer에서 정규화에 사용하는 값을 조정할 수 있습니다.')
 
+
     ############### GPU
     parser.add_argument('--device', type=str, default='cuda', choices=['cuda', 'cpu'], help='학습에 사용할 Device를 조정할 수 있습니다.')
 
-    ############### FM, FFM, NCF, WDN, DCN Common OPTION
-    parser.add_argument('--embed_dim', type=int, default=16, help='FM, FFM, NCF, WDN, DCN에서 embedding시킬 차원을 조정할 수 있습니다.')
-    parser.add_argument('--dropout', type=float, default=0.2, help='NCF, WDN, DCN에서 Dropout rate를 조정할 수 있습니다.')
-    parser.add_argument('--mlp_dims', type=list, default=(16, 16), help='NCF, WDN, DCN에서 MLP Network의 차원을 조정할 수 있습니다.')
 
-
+    ############### FM, FFM, DeepFFM, NCF, WDN, DCN Common OPTION
+    parser.add_argument('--embed_dim', type=int, default=16, help='FM, FFM, DeepFFM, NCF, WDN, DCN에서 embedding시킬 차원을 조정할 수 있습니다.')
+    parser.add_argument('--dropout', type=float, default=0.2, help='DeepFFM, NCF, WDN, DCN에서 Dropout rate를 조정할 수 있습니다.')
+    parser.add_argument('--mlp_dims', nargs='+', type=int, default=(16, 16), help='DeepFFM, NCF, WDN, DCN에서 MLP Network의 차원을 조정할 수 있습니다.')
+    
+    ############### FM, FFM, DeepFFM
+    parser.add_argument('--preprocessed', type=int, default=1, help='FM, FFM, DeepFFM에서 전처리된 데이터를 사용하는지 여부를 조정합니다. default=True')
+    
     ############### DCN
-    parser.add_argument('--num_layers', type=int, default=3, help='에서 Cross Network의 레이어 수를 조정할 수 있습니다.')
+    parser.add_argument('--num_layers', type=int, default=3, help='DCN에서 Cross Network의 레이어 수를 조정할 수 있습니다.')
 
 
     ############### CNN_FM
@@ -48,14 +54,14 @@ def define_argparser():
 
 
     ############### DeepCoNN
-    parser.add_argument('--vector_create', type=bool, default=False, help='DEEP_CONN에서 text vector 생성 여부를 조정할 수 있으며 최초 학습에만 True로 설정하여야합니다.')
+    parser.add_argument('--vector_create', type=int, default=1, help='DEEP_CONN에서 text vector 생성 여부를 조정할 수 있으며 최초 학습에만 True로 설정하여야합니다.')
     parser.add_argument('--deepconn_embed_dim', type=int, default=32, help='DEEP_CONN에서 user와 item에 대한 embedding시킬 차원을 조정할 수 있습니다.')
     parser.add_argument('--deepconn_latent_dim', type=int, default=10, help='DEEP_CONN에서 user/item/image에 대한 latent 차원을 조정할 수 있습니다.')
     parser.add_argument('--conv_1d_out_dim', type=int, default=50, help='DEEP_CONN에서 1D conv의 출력 크기를 조정할 수 있습니다.')
     parser.add_argument('--kernel_size', type=int, default=3, help='DEEP_CONN에서 1D conv의 kernel 크기를 조정할 수 있습니다.')
     parser.add_argument('--word_dim', type=int, default=768, help='DEEP_CONN에서 1D conv의 입력 크기를 조정할 수 있습니다.')
     parser.add_argument('--out_dim', type=int, default=32, help='DEEP_CONN에서 1D conv의 출력 크기를 조정할 수 있습니다.')
-
+    
     args = parser.parse_args()
     return args
 
