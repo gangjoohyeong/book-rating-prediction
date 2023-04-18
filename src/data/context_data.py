0e92760759e171db6d6a8901ce0827eea9f831b7
RonaldFisher9999 <1993sabre@gmail.com>
2023-04-18 17:45:15 +0900

    fix to make preprocessing easier

--- src/data/context_data.py
+++ src/data/context_data.py
@@ -31,50 +31,15 @@ def process_context_data(users, books, ratings1, ratings2):
                     .merge(books, on='isbn', how='left')
     test_df = ratings2.merge(users, on='user_id', how='left')\
                     .merge(books, on='isbn', how='left')
-
-    # users 인덱싱 처리
-    loc2idx = {v:k for k,v in enumerate(context_df['location'].unique())}
-    train_df['location'] = train_df['location'].map(loc2idx)
-    test_df['location'] = test_df['location'].map(loc2idx)
     
-    age2idx = {v:k for k,v in enumerate(context_df['age_bin'].unique())}
-    train_df['age_bin'] = train_df['age_bin'].map(age2idx)
-    test_df['age_bin'] = train_df['age_bin'].map(age2idx)
-
-    # book 파트 인덱싱
-    author2idx = {v:k for k,v in enumerate(context_df['book_author'].unique())}
-    train_df['book_author'] = train_df['book_author'].map(author2idx)
-    test_df['book_author'] = test_df['book_author'].map(author2idx)
-    
-    year2idx = {v:k for k,v in enumerate(context_df['year_of_publication'].unique())}
-    train_df['year_of_publication'] = train_df['year_of_publication'].map(year2idx)
-    test_df['year_of_publication'] = test_df['year_of_publication'].map(year2idx)
-    
-    publisher2idx = {v:k for k,v in enumerate(context_df['publisher'].unique())}
-    train_df['publisher'] = train_df['publisher'].map(publisher2idx)
-    test_df['publisher'] = test_df['publisher'].map(publisher2idx)
-    
-    # summary 삭제 -> 성능 안나옴
-    # train_df.drop(columns=['summary'], inplace=True)
-    # test_df.drop(columns=['summary'], inplace=True)
-    
-    category2idx = {v:k for k,v in enumerate(context_df['major_cat'].unique())}
-    train_df['major_cat'] = train_df['major_cat'].map(category2idx)
-    test_df['major_cat'] = test_df['major_cat'].map(category2idx)
-    
-    area2idx = {v:k for k,v in enumerate(context_df['isbn_area'].unique())}
-    train_df['isbn_area'] = train_df['isbn_area'].map(area2idx)
-    test_df['isbn_area'] = test_df['isbn_area'].map(area2idx)
-
-    idx = {
-        "loc2idx":loc2idx,
-        "age2idx":age2idx,
-        "author2idx":author2idx,
-        "year2idx":year2idx,
-        "publisher2idx":publisher2idx,
-        "category2idx":category2idx,
-        "area2idx":area2idx      
-    }
+    features = (context_df.drop(columns=['rating'])).columns
+    # 모든 feature 인덱싱 처리
+    idx = {feature:None for feature in features}
+    for feature in features :
+        feature2idx = {v:k for k,v in enumerate(context_df[feature].unique())}
+        idx[feature] = len(feature2idx)
+        train_df[feature] = train_df[feature].map(feature2idx)
+        test_df[feature] = test_df[feature].map(feature2idx)
 
     return idx, train_df, test_df
 
@@ -94,48 +59,19 @@ def context_data_load(args):
     books = pd.read_csv(args.data_path + 'preprocessed/books.csv')
     train = pd.read_csv(args.data_path + 'preprocessed/train_ratings.csv')
     test = pd.read_csv(args.data_path + 'preprocessed/test_ratings.csv')
-    sub = pd.read_csv(args.data_path + 'preprocessed/sample_submission.csv')
-
-    ids = pd.concat([train['user_id'], sub['user_id']]).unique()
-    isbns = pd.concat([train['isbn'], sub['isbn']]).unique()
-
-    idx2user = {idx:id for idx, id in enumerate(ids)}
-    idx2isbn = {idx:isbn for idx, isbn in enumerate(isbns)}
-
-    user2idx = {id:idx for idx, id in idx2user.items()}
-    isbn2idx = {isbn:idx for idx, isbn in idx2isbn.items()}
-
-    train['user_id'] = train['user_id'].map(user2idx)
-    sub['user_id'] = sub['user_id'].map(user2idx)
-    test['user_id'] = test['user_id'].map(user2idx)
-    users['user_id'] = users['user_id'].map(user2idx)
-
-    train['isbn'] = train['isbn'].map(isbn2idx)
-    sub['isbn'] = sub['isbn'].map(isbn2idx)
-    test['isbn'] = test['isbn'].map(isbn2idx)
-    books['isbn'] = books['isbn'].map(isbn2idx)
 
     idx, context_train, context_test = process_context_data(users, books, train, test)
     
-    # user, isbn, location, age_bin, book_author, year_of_publicaiton, publisher, summary, major_cat, isbn_area
-    field_dims = np.array([len(user2idx), len(isbn2idx),
-                           len(idx['loc2idx']), len(idx['age2idx']), 
-                           len(idx['author2idx']), len(idx['year2idx']), len(idx['publisher2idx']), 2,
-                           len(idx['category2idx']), len(idx['area2idx']),], dtype=np.uint32)
+    # print(idx)
+    field_dims = np.array(list(idx.values()), dtype=np.uint32)
+    
     print(f"field_dims: {field_dims}")
     print(f"total input dim : {sum(field_dims)}")
 
     data = {
             'train':context_train,
             'test':context_test.drop(['rating'], axis=1),
-            'field_dims':field_dims,
-            'users':users,
-            'books':books,
-            'sub':sub,
-            'idx2user':idx2user,
-            'idx2isbn':idx2isbn,
-            'user2idx':user2idx,
-            'isbn2idx':isbn2idx,
+            'field_dims':field_dims
             }
 
     return data
