640e5245c3f2df0860d72d9453925089c9e500f8
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-04-20 12:50:39 +0900

    trash: 급해서 그냥 막 넣음 곧 대회 막바지

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,3 +2,4 @@ from .context_data import context_data_load, base_context_data_load, context_dat
 from .dl_data import dl_data_load, dl_data_split, dl_data_loader
 from .image_data import image_data_load, image_data_split, image_data_loader
 from .text_data import text_data_load, text_data_split, text_data_loader
+from .cat_boost_data import load_cat_boost_data
